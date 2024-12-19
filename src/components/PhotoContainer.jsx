38ee6eb7a09adbd16e85b1d07542b8418c0ddbd5
Irem Peker <irem.peker@gmx.de>
2024-12-19 17:08:56 +0100

    Fix masonary styles

--- src/components/PhotoContainer.jsx
+++ src/components/PhotoContainer.jsx
@@ -3,10 +3,11 @@ import Masonry from "react-masonry-css";
 
 const PhotoContainer = ({ allPhotos, userName, setUserName }) => {
   const breakpointColumnsObj = {
-    default: 3,
-    1100: 3,
-    768: 2,
-    500: 1,
+    default: 4,
+    1200: 3,
+    1000: 2,
+    800: 1,
+    600: 1,
   };
 
   const photos = allPhotos?.map((el) => (
